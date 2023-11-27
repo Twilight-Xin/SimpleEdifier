0a57c15c1efd7684d833434818715a5b836ede6e
熹光 <11161842+Twilight-xin@user.noreply.gitee.com>
2023-11-27 21:36:09 +0800

    fix music prev and next malposition

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.twilight.simpleedifier"
         minSdk = 29
         targetSdk = 33
-        versionCode = 2
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
